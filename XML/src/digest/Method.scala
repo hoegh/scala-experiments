a914caac27e0c7f4ac3a82112cd1d5bafe544b95
Bjarne H. Nielsen <bjarne.h.nielsen@gmail.com>
2009-02-18 21:22:12 +0100

    Added support for parameters

--- XML/src/digest/Method.scala
+++ XML/src/digest/Method.scala
@@ -3,12 +3,12 @@ package digest
 object Method extends NodeParser[Method] {
   def parse( ancestor: FQName, node: scala.xml.Node ) = {
     val name = Name(ancestor, (node \ "@name").text)
+    val params = Param.parse( name, node \ "parameter")
     val returntype = new Reference( (node \ "@returntype").text ) 
-    
-    Method(name, returntype)
+
+    Method(name, params, returntype)
   }
 }
 
-case class Method(name: Name, returntype: Reference) {
-  
+case class Method(name: Name, params: Set[Param], returntype: Reference) {
 }
---
+++ XML/src/digest/Param.scala
@@ -0,0 +1,14 @@
+package digest
+
+object Param extends NodeParser[Param] {
+  def parse( ancestor: FQName, node: scala.xml.Node ) = {
+    val name = Name(ancestor, (node \ "@name").text)
+    val datatype = new Reference( (node \ "@type").text ) 
+    
+    Param(name, datatype)
+  }
+}
+
+case class Param(name: Name, datatype: Reference) {
+  
+}
